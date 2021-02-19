e914c4c1c3d4e0cb6c40ba07e58498d1c36a04fa
Digdarshan Kunwar <digdarshan.kunwar@gmail.com>
2021-02-19 22:31:48 +0100

    main program

--- assignment-1/main.py
+++ assignment-1/main.py
@@ -1,5 +1,63 @@
 import argparse
+import numpy as np
 from numpy import genfromtxt
+from matplotlib import pyplot as plt
+# plt.rcParams["figure.figsize"] = (25, 5)
+
+
+def morph_operation(img, SE, operation_type, direction):
+
+    if(direction == "vertical"):
+        SE = SE.reshape(SE.shape[0], 1)
+    elif(direction == "horizontal"):
+        SE = SE.reshape(1, SE.shape[0])
+
+    vmax = np.max(img)
+    vmin = np.min(img)
+
+    padding_value = None
+    operation_dilation = None
+    operation_erosion = None
+
+    # operation type either dilation or erosion
+    if(operation_type == 'd'):
+        padding_value = vmin
+        operation_dilation = True
+    elif(operation_type == 'e'):
+        padding_value = vmax
+        operation_erosion = True
+
+    SE_y = SE.shape[0]
+    SE_x = SE.shape[1]
+
+    # A flag for axes
+    turn_off_axes = False
+    if (img.shape[0] > 10 or img.shape[1] > 10):
+        turn_off_axes = True
+
+    padding_y, padding_x = SE.shape[0]//2, SE.shape[1]//2
+    img_with_boundary = np.pad(img, ((padding_y, padding_y), (padding_x, padding_x)), mode='constant',
+                               constant_values=padding_value)
+    img_subarr = []
+
+    img_output = np.zeros((img.shape[0], img.shape[1]), int)
+
+    # get the idx of the 1s in the SE
+    SE_one_idxs = np.argwhere(SE == 1)
+   # find sub-matrices inside the original matrix which are identical to the erosion matrix
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            img_subarr = img_with_boundary[i:i+SE_y, j:j+SE_x]
+            img_subarr_filtered = []
+            # iterate through the idxes 1s in the masking SE elements
+            for one_idx in SE_one_idxs:
+                img_subarr_filtered.append(img_subarr[one_idx[0], one_idx[1]])
+            if (operation_erosion):
+                img_output[i, j] = np.amin(img_subarr_filtered)
+            elif(operation_dilation):
+                img_output[i, j] = np.amax(img_subarr_filtered)
+    return img_output
+
 
 def main():
     # Create a parser
@@ -11,31 +69,40 @@ def main():
     group = parser.add_mutually_exclusive_group(required=True)
 
     group.add_argument("-d", type=str, nargs=3,
-        metavar=('SE','f','f_out'),
-        help="perform dilation\n SE : Structuring element\n f  : Input filename in CSV Format\n f_out: Output filename in CSV Format"
-    )
-        
+                       metavar=('SE', 'f', 'f_out'),
+                       help="perform dilation\n SE : Structuring element\n f  : Input filename in CSV Format\n f_out: Output filename in CSV Format"
+                       )
+
     group.add_argument("-e", type=str, nargs=3,
-        metavar=('SE','f','f_out'),
-        help="perform erosion\n SE : Structuring element\n f  : Input filename in CSV Format\n f_out: Output filename in CSV Format"
-    )
+                       metavar=('SE', 'f', 'f_out'),
+                       help="perform erosion\n SE : Structuring element\n f  : Input filename in CSV Format\n f_out: Output filename in CSV Format"
+                       )
     args = parser.parse_args()
 
-    # setting up the filenames received as an argument through command line 
-    filenames=None
+    # setting up the filenames received as an argument through command line
+    filenames = None
+    operation_type = None
+    direction = None
     if args.d:
-        filenames = args.d 
+        filenames = args.d
+        operation_type = 'd'
     elif args.e:
         filenames = args.e
-
-    print(filenames)
+        operation_type = 'e'
 
     # read the files and the CSVs
-    se = genfromtxt(filenames[0], delimiter=',').astype(int)
-    f = genfromtxt(filenames[1], delimiter=',').astype(int)
+    SE = genfromtxt(filenames[0], delimiter=',').astype(int)
+    img = genfromtxt(filenames[1], delimiter=',').astype(int)
 
-    print(se)
-    print(f)
+    if("SE2.txt" in filenames[0]):
+        direction = "vertical"
+    if("SE3.txt" in filenames[0]):
+        direction = "horizontal"
+    
+    img_output = morph_operation(img, SE, operation_type, direction)
+
+    np.savetxt(filenames[2], img_output,
+               delimiter=', ', newline='\n', fmt='%d')
 
 
 if __name__ == "__main__":
