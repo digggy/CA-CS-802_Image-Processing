4fe2595d467a2de7077a76fa1aefd3495f82cfc0
Digdarshan <d.kunwar@jacobs-university.de>
2021-03-13 19:15:09 +0100

    argparse and file input

--- assignment-2/src/watershed.py
+++ assignment-2/src/watershed.py
@@ -1,7 +1,24 @@
 import argparse
+import os.path
+import numpy as np
+from numpy import genfromtxt
+
+import matplotlib.pyplot as plt
+from skimage import color, io, data
+from skimage.color import rgb2gray
+plt.rcParams["figure.figsize"] = (20,20)
+
+def display(img):
+    # Show image
+    plt.figure(figsize = (5,5))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
 
 def main():
+    # Create a parser
     my_parser = argparse.ArgumentParser(description='Perform watershed')
+    
     my_parser.add_argument(
         '-i',
         '--input',
@@ -9,6 +26,7 @@ def main():
         metavar='PATH',
         type=str,
         help='the path to input file')
+    
     my_parser.add_argument(
         '-n'
         '--neighbours',
@@ -16,6 +34,7 @@ def main():
         metavar='N',
         type=int,
         help='number of neighbours')
+    
     my_parser.add_argument(
         '-o'
         '--output',
@@ -23,7 +42,22 @@ def main():
         metavar='PATH',
         type=str,
         help='the path to outputfile')
+    
     args = my_parser.parse_args()
+    
+    inputfile = args.input
+    if inputfile and os.path.isfile(inputfile):
+        input_image_extension = os.path.splitext(inputfile)[1]
+        img = None
+        if(input_image_extension == ".png" or input_image_extension == ".jpg" or input_image_extension == ".jgeg"):
+            img = io.imread(inputfile, as_gray=True)
+            img = img/np.max(img)*255
+            img = img.astype(np.uint8)
+        elif (input_image_extension == ".txt"):
+            img = genfromtxt(inputfile, delimiter=',').astype(int)
+            img -= np.min(img)
+            img = img/np.max(img)
+    # display(img)
     
     return 0
 
